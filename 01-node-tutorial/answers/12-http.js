295cd1746f707d01f8839823cc4472755981f390
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-07-19 21:23:29 -0700

    Improve conditions and module export based on review feedbac

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -5,13 +5,13 @@ const server = http.createServer((req, res) => {
   // Creating server
   if (req.url === "/") {
     res.end("Welcome to our homepage");
-  }
-  if (req.url === "/about") {
-    res.end("This is the about page");
-  }
-  res.end(`<h1>Bulat my friend can't find any bug</h>
+  } else if (req.url === "/about") {
+    res.end("<h1>This is the about page</h1>");
+  } else {
+    res.end(`<h1>Bulat my friend can't find any bug</h>
     <p>That is why you can't find the page you are looking for
     <a href="/">back home</a>`);
+  }
 });
 
 server.listen(3000);
