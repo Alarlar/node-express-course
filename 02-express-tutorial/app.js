556568d4b796c4d45dda05ab3daf25b9cf4d63fb
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-08-01 19:29:10 -0700

    partial content

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.static("./public"));
+
+// route, implementing api wich return json
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked!" });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
