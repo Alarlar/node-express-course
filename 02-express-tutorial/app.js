467455c6ef29a7cbd75911e44f3b7daa11ecd26b
Dinar Ibragimov <dinaribragimov@Dinar-Ibragimovs-MacBook-Air.local>
2025-08-05 12:57:04 -0700

    Handle valid POST api/people requests and return 201 status

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-let { people } = require("./data");
+let {people} = require('./data')
 
-// static assets
-app.use(express.static("./methods-public"));
-// parse form data
-app.use(express.urlencoded({ extended: false }));
+// static assets, to get the form data нужно использовать работающую программу для работы с данными
+app.use(express.static('./methods-public'))
+// parse form data - adding value to body
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ sucess: true, data: people });
-});
 
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
+app.get('/api/people', (req, res) => { // read the data
+    res.status(200).json({ scusses: true, data: people})
+})
 
-  res.status(401).send("Please Provide Credentials");
-});
+app.post('/api/people', (req, res) => { // add data
+    const {name} = req.body
+    if(!name) {
+        return res.status(400).json({ success: false, msg: 'please provide name value'})
+    }
+    res.status(201).json({success: true, person: name })
+})
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+app.post('/login', (req, res) => {
+    const {name} = req.body;
+    if(name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+
+    res.status(401).send('Please provide credentials')
+})
+
+app.listen(5001, () => {
+    console.log('Server is listening on port 5001')
+})
