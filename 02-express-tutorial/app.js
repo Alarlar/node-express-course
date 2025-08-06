0d21721d2d2f30b0cdc7ed5adcfc5db972ea8c53
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-08-05 21:40:43 -0700

    The main app.js file now uses a dedicated router to handle these endpoints

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,79 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-let {people} = require('./data')
-// const peopleRouter = require('./routes/people')
+
+const peopleRouter = require("./routes/people");
+const auth = require("./routes/auth");
 
 // static assets, to get the form data нужно использовать работающую программу для работы с данными
-app.use(express.static('./methods-public'))
+app.use(express.static("./methods-public"));
 // parse form data - adding value to body
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // parse json
-app.use(express.json())
+app.use(express.json());
 
-// app.use('/api/people', peopleRouter);
-
-app.post('/login', (req, res) => {
-    const {name} = req.body;
-    if(name) {
-        return res.status(200).send(`Welcome ${name}`)
-    }
-
-   return res.status(401).json(`Welcome ${name}`)
-})
-
-app.get('/api/people', (req, res) => { // read the data
-    res.status(200).json({ scusses: true, data: people})
-})
-
-app.post('/api/people', (req, res) => { // add data
-    const {name} = req.body
-    if(!name) {
-        return res.status(400).json({ success: false, msg: 'please provide name value'})
-    }
-    res.status(201).json({success: true, person: name })
-})
-
-app.post('/api/people/postman', (req, res) => {
-    const {name} = req.body
-    if(!name) {
-        return res
-            .status(400)
-            .json({ success: false, msg: 'please provide name value'})
-    }
-    res.status(201).json({success: true, data: [...people, name] })
-})
-
-app.put('/api/people/:id', (req, res) => {
-    const {id} = req.params // which item
-    const {name} = req.body
-    
-    const person = people.find((person) => person.id === Number(id))
-        if(!person) {
-            return res
-            .status(404)
-            .json({ success: false, msg: `no person with ${id}`})
-        }
-        const newPeople = people.map((person) => {
-            if(person.id === Number(id)){
-                person.name = name
-            }
-            return person
-        })
-        res.status(200).json({success: true, data: newPeople})
-    })
-
-    app.delete('/api/people/:id', (req, res) => {
-        const person = people.find((person) => person.id === Number(req.params.id))
-        if(!person) {
-            return res
-            .status(404)
-            .json({ success: false, msg: `no person with ${req.params.id}`})
-        }
-        const newPeople = people.filter((person) => person.id !== Number(req.params.id))
-        return res.status(200).json({success: true, data: newPeople} )
-    })
-
+app.use("/api/people", peopleRouter);
+app.use("/login", auth);
 
 app.listen(5001, () => {
-    console.log('Server is listening on port 5001')
-})
+  console.log("Server is listening on port 5001");
+});
