d0e5e1648a373fc8d6f4daff22f5f8693aa1cf8a
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-08-20 21:41:39 -0700

    working on query for crud operations

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,32 +1,35 @@
-const Task = require('../models/Task')
-console.log('Task model is:', Task)
-
+const Task = require("../models/Task");
+console.log("Task model is:", Task);
 
 const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+  res.send("get all tasks");
+};
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-}
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 const getTask = (req, res) => {
-    res.json({id: req.params.id})
-}
+  res.json({ id: req.params.id });
+};
 
 const updateTask = (req, res) => {
-    res.send('update task')
-}
+  res.send("update task");
+};
 
 const deletTask = (req, res) => {
-    res.send('delete task')
-}
+  res.send("delete task");
+};
 
-module.exports = { 
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deletTask
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deletTask,
 };
